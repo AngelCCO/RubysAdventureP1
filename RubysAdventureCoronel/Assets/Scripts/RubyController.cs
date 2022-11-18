04b87e083236666b623bcb23d23baac2b1563ee6
AngelCCO <yandelcoronel.yg@gmail.com>
2022-11-18 09:54:59 -0800

    Added Blox Colliders and interactables.
    We added to any physical object a collider including ruby while also adding some collectibles and a health bar for Ruby.

--- RubysAdventureCoronel/Assets/Scripts/RubyController.cs
+++ RubysAdventureCoronel/Assets/Scripts/RubyController.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -15,8 +27,24 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.x = position.x +3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
